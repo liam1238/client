c0eba3556887000bac43edaab902d6ea99ef776c
Liam Golan <liamgolan@Liams-MacBook-Pro.local>
2024-04-19 14:31:22 +0300

    finish client

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useConversations } from '../contexts/ConversationsProvaider';
 
 export default function Conversations() {
+  const { conversations, selectedConversationIndex } = useConversations();
   return (
-    <div>Conversations</div>
+    <ListGroup variant='flush'>
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item 
+          key={index}
+          action
+          onClick={() => selectedConversationIndex(index)}
+          active={conversation.selected}
+        >
+          {conversation.recipients.map(r => r.name).join(', ')}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   )
 }
--- src/components/Dashbord.js
+++ src/components/Dashbord.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import { useConversations } from '../contexts/ConversationsProvaider';
+import OpenConversation from './OpenConversation';
 
 
 export default function Dashbord( { id } ) {
+  const { selectedConversation } = useConversations();
+
   return (
     <div className='d-flex' style={{height: '100vh'}}>
         <Sidebar id={id}/>
+        { selectedConversation && <OpenConversation /> }
     </div>
   )
 }
